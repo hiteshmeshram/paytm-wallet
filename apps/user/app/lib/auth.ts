72a0156d1643f0aabf4a18d0b757a501abb02f16
hiteshmeshram <hiteshmeshramt@gmail.com>
2024-09-25 19:03:34 +0530

    added authentication using next-auth

---
+++ apps/user/app/lib/auth.ts
@@ -0,0 +1,59 @@
+import CredentialsProvider from "next-auth/providers/credentials"
+import bcrypt from "bcrypt";
+import  db  from "@repo/db/client"
+
+export const authOptions = {
+    providers: [
+        CredentialsProvider({
+            name: "credentials",
+            credentials: {
+                phone: { label: "phone number", type: "text", placeholder: "jsmith" },
+                password: { label: "Password", type: "password" }
+              },
+              async authorize(credentials: any) {
+                const hashedPassword = bcrypt.hash(credentials.password,10);
+                const existingUser = await db.user.findFirst({
+                    where: {
+                        number: credentials.phone
+                    }
+                })
+
+                if (existingUser) {
+                    const passwordValidations = await bcrypt.compare(credentials.password,existingUser.password);
+                    if (passwordValidations) {
+                        return {
+                            id: existingUser.id.toString(),
+                            email: existingUser.email,
+                            name: existingUser.name
+                        }
+                    } return null;
+                }
+
+                try {
+                    const user = await db.user.create({
+                        data: {
+                            number: credentials.phone,
+                            password: credentials.password
+                        }
+                    })
+                    return {
+                        id: user.id.toString(),
+                        email: user.email,
+                        name: user.name
+                    }
+                } catch(e) {
+                    console.error(e);
+                    return null;
+                }
+                
+              }
+        })
+    ],
+    secret: process.env.JWT_SECRET || "secret",
+    callbacks: {
+        async session({token,session}: any) {
+            session.userId = token.sub
+            return session;
+        }
+    }
+}
---
+++ apps/user/app/page.tsx
@@ -0,0 +1,15 @@
+import Image from "next/image";
+import { Button } from "@repo/ui/button";
+import styles from "./page.module.css";
+import { Appbar } from "./components/Appbar";
+
+
+
+export default function Home() {
+  
+  return (
+    <div className="">
+      <Appbar/>
+    </div>
+  );
+}
